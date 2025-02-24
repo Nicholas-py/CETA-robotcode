61cf6f7e7fd04c2793c1cf844cc11289244b1d81
Nicholas-py <nicholas.waslander@gmail.com>
2025-02-24 12:20:54 -0500

    motors

--- main/main.ino
+++ main/main.ino
@@ -1,7 +1,15 @@
-void setup() {
 
+const int lmotorpin = 5;
+const int rmotorpin = 4;
+
+const Servo lservo;
+const Servo rservo;
+
+void setup() {
+  lservo.attach(lmotorpin);
+  rservo.attach(rmotorpin);
 }
 
 void loop(){
-  
+
 }
