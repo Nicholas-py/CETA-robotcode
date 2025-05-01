fbae5361594eb0a9abad971e47e7ed08c40b5982
GameCobra <adrian@houser.ca>
2025-05-01 11:51:22 -0400

    Slight modifications to Adrian line following

--- main/AdrianLineFollowing.ino
+++ main/AdrianLineFollowing.ino
@@ -2,7 +2,8 @@
 
 float blackThreshhold = 0.65;
 float fullBlackThreshhold = 0.88;
-float speed = 1;
+float speed = 1.1;
+int seeBlack;
 
 struct wheelSpeeds AdrianLineFollowing(struct lightSensorReadings inputs)
 {
@@ -11,11 +12,6 @@ struct wheelSpeeds AdrianLineFollowing(struct lightSensorReadings inputs)
   if (inputs.left <= blackThreshhold && inputs.center > blackThreshhold && inputs.right <= blackThreshhold)
   {
     newMotorSpeeds = {speed, speed};
-  }
-  //All are fully black (for detecting 'T')
-  else if (inputs.left >= fullBlackThreshhold && inputs.center >= fullBlackThreshhold && inputs.right >= fullBlackThreshhold)
-  {
-    newMotorSpeeds = SLOW_TURNAROUND;
   }
   //All are black but to a lesser degree
   else if (inputs.left > blackThreshhold && inputs.center > blackThreshhold && inputs.right > blackThreshhold)
@@ -27,7 +23,7 @@ struct wheelSpeeds AdrianLineFollowing(struct lightSensorReadings inputs)
   {
     newMotorSpeeds = {0, 0.6f * speed};
   }
-  //left is write and right is black
+  //left is white and right is black
   else if (inputs.left <= blackThreshhold && inputs.right > blackThreshhold)
   {
     newMotorSpeeds = {0.6f * speed, 0};
@@ -36,6 +32,21 @@ struct wheelSpeeds AdrianLineFollowing(struct lightSensorReadings inputs)
   else
   {
     newMotorSpeeds = {-0.8f * speed, -0.8f * speed};
+  }
+
+  //All are fully black (for detecting 'T')
+  if (inputs.left >= fullBlackThreshhold && inputs.center >= fullBlackThreshhold && inputs.right >= fullBlackThreshhold)
+  {
+    seeBlack++;
+    if (seeBlack >= 3)
+    {
+      newMotorSpeeds = SLOW_TURNAROUND;
+      seeBlack = 0;
+    }
+  }
+  else
+  {
+    seeBlack = 0;
   }
 
   return newMotorSpeeds;
